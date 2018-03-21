98389e40dba16a7028e184756b6d76bd48b550cf
action-hong <kkopitehong@gmail.com>
2018-03-21 15:06:25 +0800

    修改图片及中英文适配

--- src/reducers/intl.js
+++ src/reducers/intl.js
@@ -8,7 +8,17 @@ import guiMessages from 'scratch-l10n/locales/gui-msgs';
 import paintMessages from 'scratch-l10n/locales/paint-msgs';
 import penMessages from 'scratch-l10n/locales/pen-msgs';
 
-const combinedMessages = defaultsDeep({}, guiMessages.messages, paintMessages.messages, penMessages.messages);
+// 按照这样的格式, 更改一下哈哈哈, 将所有需要翻译的全都添加进来
+// 加一个zh
+const a = {
+    en: {
+        messages: {
+            'gui.previewInfo.welcome': 'proxy haha'
+        }
+    }
+};
+
+const combinedMessages = defaultsDeep({}, guiMessages.messages, paintMessages.messages, penMessages.messages, a);
 
 Object.keys(localeData).forEach(locale => {
     // TODO: will need to handle locales not in the default intl - see www/custom-locales
